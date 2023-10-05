79c566632eb0127e2ff0f87686e2109bf9207141
piazzai <42124135+piazzai@users.noreply.github.com>
2023-10-05 23:15:28 +0200

    Style 404 page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,36 @@
 ---
 ---
 
+function rnd() {
+  "use strict";
+  return Math.floor(Math.random() * 9) + 1;
+}
+
+var t = 25, i = 0;
+
+{% for n in (1..3) %}
+{% if n == 1 %}
+{% assign max = 80 %}
+{% assign out = 4 %}
+{% elsif n == 2 %}
+{% assign max = 90 %}
+{% assign out = 0 %}
+{% else %}
+{% assign max = 100 %}
+{% assign out = 4 %}
+{% endif %}
+var l{{ n }}, s{{ n }} = document.querySelector("#n{{ n }}");
+
+l{{ n }} = setInterval(function () {
+  "use strict";
+  if (i > {{ max }}) {
+    clearInterval(l{{ n }});
+    s{{ n }}.textContent = {{ out }};
+  } else {
+    s{{ n }}.textContent = rnd(); i++;
+  }}, t)
+{% endfor %}
+
 var map = L.map("map").setView([{{ site.data.location.center | default: '47.79064, 18.95647' }}], {{ site.data.location.zoom | default: 13 }});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
