3a2528c62ec9c32ae1d2d699d3210905d5f1bbc7
piazzai <42124135+piazzai@users.noreply.github.com>
2023-10-05 13:04:33 +0200

    Change variable names

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 {%- if site.data.location.markers %}
 {%- assign reverse = site.data.location.markers | reverse %}
 {% for item in reverse %}
-L.marker([{{ item.coordinates }}])
+L.marker([{{ item.coord }}])
   .addTo(map)
-  .bindPopup("{{ item.label }}")
+  .bindPopup("{{ item.text }}")
   .openPopup();
 {% endfor %}
 {% endif %}
