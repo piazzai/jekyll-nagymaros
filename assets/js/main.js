eeacdc7e412dd932c02d53ee4bbe190cf8f59f90
piazzai <42124135+piazzai@users.noreply.github.com>
2023-10-10 13:58:55 +0200

    Make content flexible

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,24 @@ var map = L.map("map").setView([{{ site.data.locations.center | default: '47.790
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 {% if site.data.locations.markers %}
-{% assign reverse = site.data.locations.markers | reverse %}
-{% for item in reverse %}
-L.marker([{{ item.coord }}])
-  .addTo(map)
-  .bindPopup("{{ item.text }}")
-  .openPopup();
+{% for marker in site.data.locations.markers %}
+{% assign content = false %}
+{% if marker.title %}
+{% assign content = marker.title | prepend: '<strong>' | append: '</strong>' %}
+{% endif %}
+{% for line in marker.text %}
+{% if content %}
+{% assign content = content | append: '<br>' | append: line %}
+{% else %}
+{% assign content = line %}
+{% endif %}
+{% endfor %}
+L.marker([{{ marker.latlng }}])
+    {% if content %}
+    .addTo(map)
+    .bindTooltip("{{ content }}");
+    {% else %}
+    .addTo(map);
+    {% endif %}
 {% endfor %}
 {% endif %}
